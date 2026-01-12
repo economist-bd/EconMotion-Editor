30e5bbae817f9641e30cd49cf1e1c6dc5f15d2da
MD MONGERUL HAQUE <andrewlaraujo70@gmail.com>
2026-01-12 17:42:05 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // নিচের লাইনটি যোগ করুন। আপনার রিপোর নাম যদি 'video-editor' হয়, তবে '/video-editor/' লিখবেন
+  base: '/EconMotion-Editor/', 
 })
